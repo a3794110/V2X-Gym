064e97fe79a522ffe3ac45578c4e28c509b9dd83
a3794110 <a3794110@gmail.com>
2020-04-16 02:26:14 +0800

    recover HelloV2XGym

---
+++ scratch/HelloV2XGym/V2XGymConfig/sumoconfig/RouteFile/clean.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+set CD="%~dp0"
+del *.rou.xml *.rou.alt.xml
+
+pause
---
+++ scratch/HelloV2XGym/ns3sim.cc
@@ -0,0 +1,196 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2018 Piotr Gawlowicz
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Piotr Gawlowicz <gawlowicz.p@gmail.com>
+ *
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/opengym-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/node-list.h"
+#include <ns3/netanim-module.h>
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("OpenGym");
+
+
+/*
+Define observation space
+*/
+Ptr<OpenGymSpace> MyGetObservationSpace(void)
+{
+  float low = obs_low;
+  float high = obs_high;
+  std::vector<uint32_t> shape = obs_shape_vector;
+  std::string dtype = obs_dtype;
+
+  Ptr<OpenGymBoxSpace> space = CreateObject<OpenGymBoxSpace> (low, high, shape, dtype);
+  NS_LOG_UNCOND ("MyGetObservationSpace: " << space);
+  return space;
+}
+
+/*
+Define action space
+*/
+Ptr<OpenGymSpace> MyGetActionSpace(void)
+{
+
+  float low = action_low;
+  float high = action_high;
+  std::vector<uint32_t> shape = action_shape_vector;
+  std::string dtype = action_dtype;
+
+  Ptr<OpenGymBoxSpace> space = CreateObject<OpenGymBoxSpace> (low, high, shape, dtype);
+  NS_LOG_UNCOND ("MyGetActionSpace: " << space);
+  return space;
+}
+
+/*
+Define game over condition
+*/
+bool MyGetGameOver(void)
+{
+
+  bool isGameOver = false;
+  bool test = false;
+  static float stepCounter = 0.0;
+  stepCounter += 1;
+  if (stepCounter == 10 && test) {
+      isGameOver = true;
+  }
+  NS_LOG_UNCOND ("MyGetGameOver: " << isGameOver);
+  return isGameOver;
+}
+
+/*
+Collect observations
+*/
+Ptr<OpenGymDataContainer> MyGetObservation(void)
+{
+  uint32_t nodeNum = 5;
+  uint32_t low = 0.0;
+  uint32_t high = 100.0;
+  Ptr<UniformRandomVariable> rngInt = CreateObject<UniformRandomVariable> ();
+
+  std::vector<uint32_t> shape = {nodeNum,};
+  Ptr<OpenGymBoxContainer<uint32_t> > box = CreateObject<OpenGymBoxContainer<uint32_t> >(shape);
+
+  // generate random data
+  for (uint32_t i = 0; i<nodeNum; i++){
+    uint32_t value = rngInt->GetInteger(low, high);
+    box->AddValue(value);
+  }
+
+  NS_LOG_UNCOND ("MyGetObservation: " << box);
+  return box;
+}
+
+/*
+Define reward function
+*/
+float MyGetReward(void)
+{
+  static float reward = 0.0;
+  reward += 1;
+  return reward;
+}
+
+/*
+Define extra info. Optional
+*/
+std::string MyGetExtraInfo(void)
+{
+  std::string myInfo = "testInfo";
+  myInfo += "|123";
+  NS_LOG_UNCOND("MyGetExtraInfo: " << myInfo);
+  return myInfo;
+}
+
+/*
+Execute received actions
+*/
+bool MyExecuteActions(Ptr<OpenGymDataContainer> action)  // set the contention window size
+{
+  NS_LOG_UNCOND ("MyExecuteActions: " << action);
+
+  Ptr<OpenGymBoxContainer<uint32_t> > box = DynamicCast<OpenGymBoxContainer<uint32_t> >(action);
+  std::vector<uint32_t> actionVector = box->GetData();
+
+  uint32_t nodeNum = NodeList::GetNNodes ();
+  for (uint32_t i=0; i<nodeNum; i++)
+  {
+    Ptr<Node> node = NodeList::GetNode(i);
+   
+  }
+
+  return true;
+}
+
+
+void ScheduleNextStateRead(double envStepTime, Ptr<OpenGymInterface> openGym)
+{
+  Simulator::Schedule (Seconds(envStepTime), &ScheduleNextStateRead, envStepTime, openGym);
+  openGym->NotifyCurrentState();
+}
+
+int
+main (int argc, char *argv[])
+{
+
+  V2XGym_InitialParameters();
+  //cmd.AddValue... need to add between here
+  V2XGym_SettingConfigParameters();
+  
+  Ptr<NetworkSetting> NetSet = CreateObject<NetworkSetting>();
+  NetSet->IncludeConfig(NetworkConfig);
+  std::string HelloV2XGym;
+  V2XGym_RegisterParameters(HelloV2XGym, string, NetSet);
+  NS_LOG_UNCOND("HelloV2XGym (set from config files): "<<HelloV2XGym);
+  RngSeedManager::SetSeed (1);
+  RngSeedManager::SetRun (simSeed);
+
+
+  // Create Nodes
+  V2XGym_InitCVs();
+
+  // OpenGym Env
+  Ptr<OpenGymInterface> openGym = CreateObject<OpenGymInterface> (openGymPort);
+  openGym->SetGetActionSpaceCb( MakeCallback (&MyGetActionSpace) );
+  openGym->SetGetObservationSpaceCb( MakeCallback (&MyGetObservationSpace) );
+  openGym->SetGetGameOverCb( MakeCallback (&MyGetGameOver) );
+  openGym->SetGetObservationCb( MakeCallback (&MyGetObservation) );
+  openGym->SetGetRewardCb( MakeCallback (&MyGetReward) );
+  openGym->SetGetExtraInfoCb( MakeCallback (&MyGetExtraInfo) );
+  openGym->SetExecuteActionsCb( MakeCallback (&MyExecuteActions) );
+  Simulator::Schedule (Seconds(0.0), &ScheduleNextStateRead, envStepTime, openGym);
+
+
+  NS_LOG_UNCOND ("Simulation start");
+  Simulator::Stop (Seconds (simulationTime));
+
+  AnimationInterface anim("ns3gym-mobility.xml"); 
+
+  Simulator::Run ();
+  NS_LOG_UNCOND ("Simulation stop");
+
+  openGym->NotifySimulationEnd();
+  Simulator::Destroy ();
+
+}
+
