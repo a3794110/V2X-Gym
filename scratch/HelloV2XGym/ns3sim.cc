29d7e5786a10050a5c9d1ba37f7a03545e1d2e3d
a3794110 <a3794110@gmail.com>
2020-04-17 05:24:07 +0800

    add lincense info

--- scratch/HelloV2XGym/ns3sim.cc
+++ scratch/HelloV2XGym/ns3sim.cc
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2018 Piotr Gawlowicz
+ * Copyright (c) 2020 Ping-Han Hsieh
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Piotr Gawlowicz <gawlowicz.p@gmail.com>
+ * Author: Ping-Han Hsieh <a3794110@gmail.com>
  *
  */
 
